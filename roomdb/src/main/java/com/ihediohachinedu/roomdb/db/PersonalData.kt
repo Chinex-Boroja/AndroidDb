98d5fef1634c89c551d47d524063b804450f3afc
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-05-24 11:24:45 +0100

    data class

--- roomdb/src/main/java/com/ihediohachinedu/roomdb/db/PersonalData.kt
+++ roomdb/src/main/java/com/ihediohachinedu/roomdb/db/PersonalData.kt
@@ -1,4 +1,25 @@
 package com.ihediohachinedu.roomdb.db
 
-class PersonalData {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity (tableName = PersonalData.TABLE_NAME)
+data class PersonalData(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = ID)
+    var userID: Int? = null,
+    @ColumnInfo(name = NAME)
+    var fullName: String? = null ,
+    @ColumnInfo(name = AGE)
+    var ageTotal: Int? = null
+
+) {
+    companion object{
+        const val TABLE_NAME = "personal_details"
+        const val ID = "id"
+        const val NAME = "name"
+        const val AGE = "age"
+    }
 }
