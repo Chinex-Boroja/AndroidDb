25d0544ec85e5703c5e20407f0e152186e4c44d0
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-05-24 11:23:50 +0100

    PersonalData.kt

--- roomdb/src/main/java/com/ihediohachinedu/roomdb/db/PersonalDataDb.kt
+++ roomdb/src/main/java/com/ihediohachinedu/roomdb/db/PersonalDataDb.kt
@@ -1,4 +1,31 @@
 package com.ihediohachinedu.roomdb.db
 
-class PersonalDataDb {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [PersonalData::class], version = DB_VERSION)
+abstract class PersonalDataDb: RoomDatabase() {
+
+    abstract fun personalDataDao(): PersonalDataDao
+
+    companion object {
+        @Volatile
+        private var databaseInstance: PersonalDataDb? = null
+
+        fun getDatabaseInstance(context: Context): PersonalDataDb =
+            databaseInstance ?: synchronized(this) {
+                databaseInstance ?: buildDatabaseInstance(context).also {
+                    databaseInstance = it
+                }
+            }
+
+        private fun buildDatabaseInstance(context: Context) =
+            Room.databaseBuilder(context, PersonalDataDb::class.java, DB_NAME)
+                .fallbackToDestructiveMigration().build()
+    }
 }
+
+const val DB_NAME = "PersonalDataSample.db"
+const val DB_VERSION = 1
