dd9de1b2a77adc75a668ce8daf4bad4d2be7523e
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-05-24 11:24:20 +0100

    PersonalDao class

--- roomdb/src/main/java/com/ihediohachinedu/roomdb/db/PersonalDataDao.kt
+++ roomdb/src/main/java/com/ihediohachinedu/roomdb/db/PersonalDataDao.kt
@@ -1,4 +1,21 @@
 package com.ihediohachinedu.roomdb.db
 
+import androidx.room.*
+import io.reactivex.Completable
+import io.reactivex.Single
+
+@Dao
 interface PersonalDataDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPersonData(data: PersonalData) : Completable
+
+    @Query("SELECT * FROM ${PersonalData.TABLE_NAME}")
+    fun getAllRecords(): Single<List<PersonalData>>
+
+    @Delete
+    fun deletePersonalData(personalData: PersonalData) : Completable
+
+    @Update
+    fun updatePersonalData(personalData: PersonalData)
 }
